450b8feb8fdd31421c21ec5aad00be1342f12972
dashrath24 <dashrathbhil24@gmail.com>
2025-05-31 17:32:35 +0530

    new project added

--- 12Lecture-contact-manager/models/Contact.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const contactSchema = new mongoose.Schema({
-  contactName: String,
-  phone: Number,
-  email: String,
-  tag: String,
-  lastContacted: Date,
-});
-module.exports = mongoose.model("Contact", contactSchema);
